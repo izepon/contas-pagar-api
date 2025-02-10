89c19d7bcc4eedacbcce11e858474666130a3b6e
jean-izepon <izeponjc@gmail.com>
2025-02-10 15:01:11 -0300

    ajuste no migration

--- src/main/resources/db/migration/V1__create_conta_table.sql
+++ src/main/resources/db/migration/V1__create_conta_table.sql
@@ -1,8 +1,10 @@
+CREATE TYPE situacao_conta AS ENUM ('PENDENTE', 'PAGO', 'VENCIDO');
+
 CREATE TABLE contas (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     data_vencimento DATE NOT NULL,
     data_pagamento DATE,
     valor DECIMAL(10, 2) NOT NULL,
     descricao TEXT NOT NULL,
-    situacao VARCHAR(20) NOT NULL
+    situacao situacao_conta NOT NULL
 );
