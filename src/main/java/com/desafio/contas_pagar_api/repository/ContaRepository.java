429fc0eff4bf0cf983026897db8e98bb737a4dc7
jean-izepon <izeponjc@gmail.com>
2025-02-10 13:12:24 -0300

    adicionado repository

--- src/main/java/com/desafio/contas_pagar_api/repository/ContaRepository.java
+++ src/main/java/com/desafio/contas_pagar_api/repository/ContaRepository.java
@@ -1,4 +1,20 @@
 package com.desafio.contas_pagar_api.repository;
 
-public class ContaRepository {
+import com.desafio.contas_pagar_api.model.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    List<Conta> findByDataVencimentoBetweenAndDescricaoContaining(LocalDate startDate, LocalDate endDate, String descricao);
+
+    Optional<Conta> findById(Long id);
+
+    BigDecimal sumByDataPagamentoBetween(LocalDate startDate, LocalDate endDate);
 }
