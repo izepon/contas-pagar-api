dbfc5dcdee03f7d86e36971d9bcfe22ef4e3d448
jean-izepon <izeponjc@gmail.com>
2025-02-10 19:53:42 -0300

    ajuste no banco

--- src/main/resources/db/migration/V1__create_conta_table.sql
+++ src/main/resources/db/migration/V1__create_conta_table.sql
@@ -1,7 +1,11 @@
 CREATE TYPE situacao_conta AS ENUM ('PENDENTE', 'PAGO', 'VENCIDO');
 
+CREATE SEQUENCE contas_id_seq
+START WITH 1
+INCREMENT BY 1;
+
 CREATE TABLE contas (
-    id BIGINT PRIMARY KEY,
+    id BIGINT PRIMARY KEY DEFAULT nextval('contas_id_seq'::regclass),
     data_vencimento DATE NOT NULL,
     data_pagamento DATE,
     valor DECIMAL(10, 2) NOT NULL,
